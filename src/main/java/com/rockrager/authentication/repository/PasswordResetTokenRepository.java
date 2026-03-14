0a5ab1a178684bf6a0dc76354f7b0f5564be77cc
mohankumaronly <mohankumaronly81@gmail.com>
2026-03-14 14:55:57 +0530

    repository files are updated

---
+++ src/main/java/com/rockrager/authentication/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,14 @@
+package com.rockrager.authentication.repository;
+
+import com.rockrager.authentication.entity.PasswordResetToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface PasswordResetTokenRepository
+        extends JpaRepository<PasswordResetToken, UUID> {
+
+    Optional<PasswordResetToken> findByToken(String token);
+
+}
---
+++ src/main/java/com/rockrager/authentication/repository/RefreshTokenRepository.java
@@ -0,0 +1,16 @@
+package com.rockrager.authentication.repository;
+
+import com.rockrager.authentication.entity.RefreshToken;
+import com.rockrager.authentication.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID> {
+
+    Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
+
+}
