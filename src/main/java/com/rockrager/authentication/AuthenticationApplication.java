225f24623d9f7d20f8993490d0395d79f0ba1987
mohankumaronly <mohankumaronly81@gmail.com>
2026-03-15 15:06:26 +0530

    main file is updated

--- src/main/java/com/rockrager/authentication/AuthenticationApplication.java
+++ src/main/java/com/rockrager/authentication/AuthenticationApplication.java
@@ -1,5 +1,6 @@
 package com.rockrager.authentication;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AuthenticationApplication {
 
 	public static void main(String[] args) {
+
+		// Load .env file
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		// Set variables as system properties so Spring can read them
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(AuthenticationApplication.class, args);
 	}
-
 }
